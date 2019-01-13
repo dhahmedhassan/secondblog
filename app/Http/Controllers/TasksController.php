97c210794b3ce97b91bc8997893b143841a3421c
Ahmed Hassan <dhahmedhassan@protonmail.com>
2019-01-13 16:13:47 +0200

    route model bind Task model

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -16,11 +16,8 @@ class TasksController extends Controller
         return view('tasks.index', compact('tasks'));
     }
 
-    public function show($id)
+    public function show(Task $task)
     {
-        $task = Task::find($id); // find() is to search in database
-
-        // return $tasks; // this returns JASON format
         return view('tasks.show', compact('task'));
     }
 }
