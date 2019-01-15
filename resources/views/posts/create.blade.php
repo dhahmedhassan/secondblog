0cd4d074d9abe9a78004cda79da064a63a29c305
Ahmed Hassan <dhahmedhassan@protonmail.com>
2019-01-15 18:38:54 +0200

    add input fields to posts.create view

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,7 +1,28 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="col-sm-8 blog-main">
         <h1>Create a Post</h1>
-    </div>
+
+        <hr>
+
+        <form action="/posts" method="post">
+            {{ csrf_field() }}
+        
+            <div class="form-group">
+                <label for="title">Title </label>
+                <input type="text" name="title" id="title" class="form-control" placeholder="Title Here" aria-describedby="titleId" maxlength="60">
+                <small id="titleId" class="text-muted">Title for the Post</small>
+            </div>
+
+            <div class="form-group">
+              <label for="body">Post Body</label>
+              <textarea class="form-control" name="body" id="body"></textarea>
+            </div>
+
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">Publish</button>
+            </div>
+
+        </form>
+
 @endsection
